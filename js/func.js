5923da5aa50c0ec005f46a30884aaad1c28ee461
nicolas barra <nicolasbarrapelecano@gmail.com>
2021-10-04 19:17:13 -0300

    nueva version

---
+++ js/func.js
@@ -0,0 +1,147 @@
+function inicioTanda1(){//utilizamos jquery para incorporar elemntos al dom
+  $('#quest1').html ('<button id="1" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " > Siento un temor profundo que me produce grandes trastornos y hasta pánico </button><button id="2" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " > Me preocupan mis propias reacciones, temo descontrolarme y actuar con violencia.</button> <button id="3" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " > Alguna situación que conozco me produce temor, me transforma emocionalmente y lo evito.</button> <button id="4 "class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " > Siento temores vagos inexplicables que me vuleven sumamente aprehensivo.</button> <button id="5"  class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " > Siento exagerada preocupacion por otras personas, lo que me produce ansiedad y angustia.</button>');
+
+  // Obtener referencia a botones por medio de la clase
+const botones = document.querySelectorAll(".pregunta");
+const botonClick = function (evento) {
+	// Recuerda, this es el elemento
+	let id=parseInt( this.id);//convierto a entero el valor del id
+    const findGrupo1 = grupoTemor.find(flor=>flor.id == id );
+    resultado.push(findGrupo1);
+    console.log(findGrupo1);
+    inicioTanda2();
+    $('#quest1').html("");
+}
+// botones es un array así que lo recorremos
+botones.forEach(boton => {
+	//Agregar listener
+	boton.addEventListener("click", botonClick);
+});
+}
+
+function inicioTanda2(){
+  $('#quest2').html ('<button id="1" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " > Prefiero la soledad, aunque a veces me hace sufrir y me pone tenso </button><button id="2" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " > Soy demasiado impulsivo e impaciente y no soporto la lentitud de los demás.</button> <button id="3" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " > Hablo mucho y me cuesta escuchar.</button>');
+
+  // Obtener referencia a botones por medio de la clase
+const botones = document.querySelectorAll(".pregunta");
+const botonClick = function (evento) {
+	// Recuerda, this es el elemento
+	let id=parseInt( this.id);//convierto a entero el valor del id
+    const findGrupo2 = grupoSoledad.find(flor=>flor.id == id );
+    resultado.push(findGrupo2);
+    console.log(findGrupo2);
+    $('#quest2').html("");
+    inicioTanda3();
+}
+// botones es un array así que lo recorremos
+botones.forEach(boton => {
+	//Agregar listener
+	boton.addEventListener("click", botonClick);
+});
+}
+
+function inicioTanda3(){
+  $('#quest3').html ('<button id="1" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Cambio de planes demasiado seguido, siempre busco consejos de otros y ello me quita seguridad y confianza.</button><button id="2" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Las dudas me dominan, soy indeciso y vacilante.</button> <button id="3" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Me desanimo con facilidad, abandono algunas cosas si no obtengo resuletados rápidamente y ello me deprime.</button> <button id="4 "class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Me siento deprimido y desesperanzado, Ya nada va a cambiar.</button> <button id="5"  class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Estoy inseguro de mis propias fuerzas y siento cansancio mental</button><button id="6"  class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Quiero hacer algo con sentido, pero no encuentro mi vocación.</button>')
+
+  // Obtener referencia a botones por medio de la clase
+const botones = document.querySelectorAll(".pregunta");
+const botonClick = function (evento) {
+	// Recuerda, this es el elemento
+	let id=parseInt( this.id);//convierto a entero el valor del id
+    const findGrupo3 = grupoIncertidumbre.find(flor=>flor.id == id );
+    resultado.push(findGrupo3);
+    console.log(findGrupo3);
+    inicioTanda4();
+    $('#quest3').html("");
+}
+// botones es un array así que lo recorremos
+botones.forEach(boton => {
+	//Agregar listener
+	boton.addEventListener("click", botonClick);
+});
+}
+
+function inicioTanda4(){
+  $('#quest4').html ('<button id="1" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Soy un soñador que a veces me evado de la realidad y me hace vivir desconectado del presente.</button><button id="2" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Estoy aferrado al pasado y a sus recuerdos, y esto no me permite encarar el presente.</button> <button id="3" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Acepto las fataidades del destino, pierdo la esperanza y me resigno a no cambiar.</button> <button id="4 "class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Me siento agotado, muy cansado fisicamente.</button> <button id="5"  class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Mi mente nunca se apaga, es com oun disco que se repite y no me deja descansar.</button><button id="6"  class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Me siento deprimido, pero al tiempo se me pasa, es por ciclos.</button><button id="7"  class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Me apresuro demasiado y repito errores por no tomar en cuenta el resultado de experiencias anteriores.</button>')
+
+  // Obtener referencia a botones por medio de la clase
+const botones = document.querySelectorAll(".pregunta");
+const botonClick = function (evento) {
+	// Recuerda, this es el elemento
+	let id=parseInt( this.id);//convierto a entero el valor del id
+    const findGrupo4 = grupoFaltaInteres.find(flor=>flor.id == id );
+    resultado.push(findGrupo4);
+    console.log(findGrupo4);
+    inicioTanda5();
+    $('#quest4').html ("");
+}
+// botones es un array así que lo recorremos
+botones.forEach(boton => {
+	//Agregar listener
+	boton.addEventListener("click", botonClick);
+});
+}
+
+function inicioTanda5(){
+  $('#quest5').html ('<button id="1" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Escondo mis problemas detras de una sonrisa, y ello me angustia.</button><button id="2" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Estoy predispuesto para atender a los demas, me cuesta decir que NO.</button> <button id="3" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Me influencian los cambios y algunas personas debido a mi sensibilidad.</button> <button id="4 "class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Me siento enojado, celoso y quiero revancha.</button>');
+
+  // Obtener referencia a botones por medio de la clase
+const botones = document.querySelectorAll(".pregunta");
+const botonClick = function (evento) {
+	// Recuerda, this es el elemento
+	let id=parseInt( this.id);//convierto a entero el valor del id
+    const findGrupo5 = grupoHipersensibilidad.find(flor=>flor.id == id );
+    resultado.push(findGrupo5);
+    console.log(findGrupo5);
+    inicioTanda6();
+    $('#quest5').html ("")
+}
+// botones es un array así que lo recorremos
+botones.forEach(boton => {
+	//Agregar listener
+	boton.addEventListener("click", botonClick);
+});
+}
+
+function inicioTanda6(){
+  $('#quest6').html ('<button id="1" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Tengo poca fe en mis capacidades.</button><button id="2" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Me reprocho cosas, me siento culpable y me culpo a mi mismo.</button> <button id="3" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Me siento abrumado por las responsabilidades y ello me deprime</button> <button id="4 "class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Me siento en el limite de mi resistencia, tengo angustia, desesperanza y sensacion de soledad.</button><button id="5" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Estoy sufriendo los efectos de un duelo.</button><button id="6" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >No es muy justo lo que me pasa y por eso estoy triste, amargo e irritable.</button> <button id="7" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Tengo un exagerado sentido del deber, me exijo demasiado sin admitir cansancio.</button> <button id="8 "class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Me siento sucioy me averguenza mi apariencia</button>');
+
+  // Obtener referencia a botones por medio de la clase
+const botones = document.querySelectorAll(".pregunta");
+const botonClick = function (evento) {
+	// Recuerda, this es el elemento
+	let id=parseInt( this.id);//convierto a entero el valor del id
+    const findGrupo6 = grupoDesaliento.find(flor=>flor.id == id );
+    resultado.push(findGrupo6);
+    console.log(findGrupo6);
+    inicioTanda7();
+    $('#quest6').html("");
+}
+// botones es un array así que lo recorremos
+botones.forEach(boton => {
+	//Agregar listener
+	boton.addEventListener("click", botonClick);
+});
+}
+
+function inicioTanda7(){
+  $('#quest7').html ('<button id="1" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Tengo tendencia a ser posesivo y a sentirme ofendido con facilidad. Me cuesta separarme de mi familia.</button><button id="2" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Soy un poco fanático, con ideas fijas que me cuesta cambiar, mi entusiasmo por hacer cosas me deja agotado.</button> <button id="3" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >A veces me siento omnipotente, lo que me mantiene en tension. Suelo ser mandon y dominante.</button> <button id="4 "class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Soy demasiado rigido, crítico e intolerante con los demás.</button><button id="5" class="pregunta bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-base " >Soy demasiado rígido conmigo mismo, bueso la pertfección en todo.</button>');
+
+  // Obtener referencia a botones por medio de la clase
+const botones = document.querySelectorAll(".pregunta");
+const botonClick = function (evento) {
+	// Recuerda, this es el elemento
+	let id=parseInt( this.id);//convierto a entero el valor del id
+    const findGrupo7 = grupoDesaliento.find(flor=>flor.id == id );
+    resultado.push(findGrupo7);
+    console.log(findGrupo7);
+    $('#quest7').html ("");
+    localStorage.setItem('contenidoFrasco',JSON.stringify(resultado));
+    mostrarResultado();
+}
+// botones es un array así que lo recorremos
+botones.forEach(boton => {
+	//Agregar listener
+	boton.addEventListener("click", botonClick);
+});
+}
---
+++ js/main.js
@@ -0,0 +1,100 @@
+//la idea basica es que el paciente ingrese de cada menu un valor que lo identifique con sus sensaciones y el sistema le arme la formula ideal segun los valores ingresados
+
+const grupoTemor = [
+    {id: 1, flor: "Rock Rose"},
+    {id: 2, flor: "Cherry Plum"},
+    {id: 3, flor: "Mimulus"},
+    {id: 4, flor: "Aspen"},
+    {id: 5, flor: "Red Chesnut"},
+]
+
+const grupoSoledad = [
+    {id: 1, flor: "Water Violet"},
+    {id: 2, flor: "Impatiens"},
+    {id: 3, flor: "Heather"},
+]
+
+const grupoIncertidumbre = [
+    {id: 1, flor: "Cerato"},
+    {id: 2, flor: "Scleranthus"},
+    {id: 3, flor: "Gentian"},
+    {id: 4, flor: "Gorse"},
+    {id: 5, flor: "Hornbeam"},
+    {id: 6, flor: "Wild Oat"},
+]
+const grupoFaltaInteres = [
+    {id:1, flor: "Clematis"},
+    {id:2, flor: "Honey Suckle"},
+    {id:3, flor: "Wild Rose"},
+    {id:4, flor: "Olive"},
+    {id:5, flor: "White Chesnut"},
+    {id:6, flor: "Mustard"},
+    {id:7, flor: "Chesnut Bud"},
+]
+
+const grupoHipersensibilidad = [
+    {id:1, flor: "Agrimony"},
+    {id:2, flor: "Centaury"},
+    {id:3, flor: "Walnut"},
+    {id:4, flor: "Holly"},
+]
+
+const grupoDesaliento = [
+    {id:1, flor: "Larch"},
+    {id:2, flor: "Pine"},
+    {id:3, flor: "Elm"},
+    {id:4, flor: "Sweet Chestnut"},
+    {id:5, flor: "Star of Bethlehem"},
+    {id:6, flor: "Willow"},
+    {id:7, flor: "Oak"},
+    {id:8, flor: "Crab Apple"},
+]
+
+const grupoPreocupacion = [
+    {id:1, flor: "Chicory"},
+    {id:2, flor: "Verbain"},
+    {id:3, flor: "Vine"},
+    {id:4, flor: "Beech"},
+    {id:5, flor: "Rock Water"},
+]
+
+
+const resultado = []
+
+$('#inicio').on('click', function(){//evento manejado por jquery
+    $('#inicio').html ("");
+    inicioTanda1();
+    
+})
+
+/* let cuestionario1 = $('#quest1'); */
+let cuestionario2 = $('#quest2');
+let cuestionario3 = $('#quest3');
+let cuestionario4 = $('#quest4');
+let cuestionario5 = $('#quest5');
+let cuestionario6 = $('#quest6');
+let cuestionario7 = $('#quest7');
+
+
+function dataCompuesto(){//funcion  para concatenar los datos y mostrarlos por pantalla
+    const jsonResultado = JSON.parse(localStorage.getItem('contenidoFrasco'));
+    frasco = ("Su combinacion ideal segun el test esta formada por: "+ jsonResultado[0].flor + " + " + jsonResultado[1].flor + " + " + jsonResultado[2].flor + " + " + jsonResultado[3].flflor + " +" + jsonResultado[4].flor + " + " + jsonResultado[5].flor +" + " + jsonResultado[6].flor) 
+    localStorage.setItem('frascoGuardado', frasco);
+}
+function mostrarResultado() {
+  let mostrarResultado = document.createElement ("div"); //creo un nuevo div para insertar en el html
+    mostrarResultado.setAttribute ("class", "resultado");// le agrego una clase al div creado arriba
+    dataCompuesto();
+    mostrarResultado.innerHTML = localStorage.getItem('frascoGuardado');
+    document.body.appendChild( mostrarResultado ); // inserto el div creado con los datos
+    
+    let repetir = document.createElement('div');
+    repetir.setAttribute("class", "grid place-content-center my-5");
+    repetir.innerHTML = '<button id="reiniciar" class="bg-pink-500 hover:bg-pink-300 text-white font-bold px-4 py-2 rounded text-lg">Reiniciar </button>';
+    document.body.appendChild (repetir);
+
+    let reinicia = document.getElementById('reiniciar');
+    reinicia.onclick=()=>{
+    location.reload();
+}
+}
