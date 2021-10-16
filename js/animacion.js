195dfa91af89ba86745cdd4e92a9533361e7aab4
nicolas barra <nicolasbarrapelecano@gmail.com>
2021-10-16 12:43:13 -0300

    final

--- js/animacion.js
+++ js/animacion.js
@@ -4,6 +4,5 @@ $("#inicio").click(function(){
 
 $('#logoInit').css({'height':'1000', 'border-style':'dashed','border-color':'pink'});
 $('#logoInit').animate({'height':150}, 2000);
- // Hace crecer la letra a tamaño 30 y pone el grosor del borde a 5 en un segundo
 $('#logoInit').animate({'border-width':5},1000);
 
